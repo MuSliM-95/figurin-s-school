4e20804b62b4ec07e63b336adf6f7f7c85541395
AgReSSoR <samahkinets@gmail.com>
2023-01-26 01:09:29 +0500

    Телеграм бот для частной школы

--- src/certificateGenerator.js
+++ src/certificateGenerator.js
@@ -1,44 +1,42 @@
 const sharp = require('sharp');
-const path = require('path')
-const fs = require('fs')
-
-let folderPath = fs.readdirSync("./image/dataChange").map(fileName => {
-    return path.join(folderPath, fileName)
-  })
-console.log(folderPath);
-
-let img = "./image/img.png"
-const addName = async (name) => {
+    
+ const addName = async ( name, username) => {
   const svgText = `
-  <svg width="1600" height="1100">
+  <svg width="2000" height="1100">
     <style>
-      .title { fill: bleck; font-size: 130px}
+      .title { fill: bleck; font-size: 100px}
     </style>
-    <text x="50%" y="45%" text-anchor="middle" class="title">${name}</text>
+    <text x="49%" y="47%" text-anchor="middle" class="title">${name}</text>
+   
   </svg>`
 
   const svgBuffer = await Buffer.from(svgText);
 
- await sharp (__dirname + img)
+ await sharp (`${__dirname}./image/dataChange/data.png`)
   .composite([{input: svgBuffer}])
-  .toFile(__dirname + "./certificate/certificate.png")
+  .toFile(`${__dirname}./image/certificate/${username}.png`)
 }
 
-const addDate = async (date) => {
-const svgText = `<svg width="1600" height="1100">
+const addDate = async (text1) => {
+const svgText = `<svg width="2900" height="2100">
 <style>
-  .title { fill: bleck; font-size: 130px}
+  .title { fill: #404e58; font-size: 75px}
 </style>
-<text x="50%" y="45%" text-anchor="middle" class="title">${date}</text>
+<text x="51.1%" y="70.50%" text-anchor="middle" class="title">${text1}</text>
+
 </svg>`
 const  svgBuffer = await Buffer.from(svgText)
-await sharp (__dirname + img)
- ``
+await sharp (`${__dirname}./image/img.png`)
+
 .composite([{input: svgBuffer}])
- `./image/img${Math.floor(Math.random() * 1000)}.png`
-.toFile(`${__dirname}./image/img${Math.floor(Math.random() * 1000)}.png`)
+.toFile(`${__dirname}./image/dataChange/data.png`)
+
+img = "./image/data.png"
 
 }
+ 
+module.exports = {addDate, addName} 
+// addDate('c 23 по 25 Февряль 2023 года' )  
+// addName("Архиев", "Магомед", "Хадидович", "@HeIIoW0RID" ) 
 
-// addDate("--------------------")
-// addName("Архиев Магомед Хадидович")
+  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api")
-const { KeyboardContainer, keyboardRemove } = require("./options")
+const { KeyboardContainer, keyboardRemove, KeyboardAdmin } = require("./options")
+const data = require("../date.json")
+const { addName, addDate } = require("./certificateGenerator")
+const fs = require("fs")
 
 require("dotenv").config()
 
@@ -8,31 +11,83 @@ const bot = new TelegramBot(process.env.TOKEN, {
 })
 
 bot.setMyCommands([
-    {command:"/start", description:"Начать работу"},
-    {command:"/keyboard", description:"Запустить помощник"},
+    { command: "/start", description: "Начать работу" },
+    { command: "/keyboard", description: "Запустить помощник" },
 ])
 
+let admin = false
+let user = false
 
 bot.on("message", async msg => {
 
-const { id, first_name, username } = msg.chat
+    const { id, first_name, username } = msg.chat
 
 
-console.log(msg);
     switch (msg.text) {
         case "/start":
-         await  bot.sendMessage(id, `Здравстуйте ${first_name}`, KeyboardContainer)
+            await bot.sendMessage(id, `Здравстуйте ${first_name}`, username === "HeIIoW0RID" ? KeyboardAdmin : KeyboardContainer)
+            admin = false
             break;
         case "/keyboard":
-         await  bot.sendMessage(id, `Помощник открыт!`, KeyboardContainer)
+            await bot.sendMessage(id, `Помощник открыт!`, username === "HeIIoW0RID" ? KeyboardAdmin : KeyboardContainer)
+            admin = false
             break;
         case "Закрыть":
             await bot.sendMessage(id, "Помощник закрыт!", keyboardRemove)
+            admin = false
             break;
-        default: 
+        case "Получить сертификат":
+            certificate(data, id, username)
+            admin = false
+            break;
+        case "Изменить сертификат":
+            if (username === "HeIIoW0RID") {
+                await bot.sendMessage(id, "Ваша следующая сообщение будет принят как изменение")
+                admin = true
+                // user = false 
+            }
+            break;
+        case "Получить сертификат ученика":
+            if (username === "HeIIoW0RID") {
+                await bot.sendMessage(id, "Ведите username ученика")
+                user = true
+                admin = false
+            }
+            break;
+        default:
+            if(admin) {
+                addDate(msg.text)
+                admin = false
+               await bot.sendMessage(id, "Идет загрузка сертификата...")
+                setTimeout(() =>  {
+                 bot.sendPhoto(id, __dirname +"/image/dataChange/data.png")
+               },3000)
+                setTimeout(() =>  {
+                 bot.sendMessage(id," Успешно изменен")
+               },4000)
+               
+            } else if(user) {
+               certificate(data, id, msg.text)
+               user = false
+            }
             break;
 
     }
 
+    async function certificate(data, chatId, username) {
+        await data.users.map(el => {
+            if (el.username === username) {
+                addName(`${el.surname} ${el.name} ${el.patronymic}`, el.username)
+                bot.sendMessage(chatId, "Загружаем сертификат...")
+                setTimeout(() => {
+                    bot.sendPhoto(chatId, `${__dirname}/image/certificate/${el.username}.png`)
+                }, 3000)
+
+
+            }
+        })
+    }
+
+    
 
 })
